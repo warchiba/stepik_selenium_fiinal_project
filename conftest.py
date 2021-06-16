ccc9f2561eda69f8335ec410481f62efe863e703
Кира Кузнецова <kekuznetsova_1@miem.hse.ru>
2021-06-16 17:26:17 +0300

    add implicity wait

--- conftest.py
+++ conftest.py
@@ -13,6 +13,7 @@ def browser(request):
 	options = Options()
 	options.add_experimental_option('prefs', {'intl.accept_languages': language})
 	browser = webdriver.Chrome('chromedriver.exe', options=options)
+	browser.implicitly_wait(10)
 	yield browser
 	print("\nquit browser..")
 	browser.quit()
